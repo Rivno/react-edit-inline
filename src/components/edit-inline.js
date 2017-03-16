4b38cda79ac7311a9c8c95331ad9c489418e415f
rivno <abienne@gmail.com>
2017-03-16 14:55:26 +0100

    init du repo

---
+++ src/components/edit-inline.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+import './edit-inline.css';
+
+class EditInline extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            isEdit: false
+        };
+    }
+
+    edit() {
+        this.setState({isEdit: !this.state.isEdit});
+    }
+
+    onKeyUp(e) {
+        if (e.keyCode === 13) {
+            this.edit();
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.state.isEdit === true && this.props.onEditEnter) {
+            this.props.onEditEnter();
+        }
+
+        if (this.state.isEdit === false && this.props.onEditLeave) {
+            this.props.onEditLeave();
+        }
+    }
+
+    render() {
+        const cssClass = `react-edit-inline ${this.props.className ? this.props.className : ''}`;
+
+        if (this.state.readOnly) {
+            return (
+                <div className={cssClass}>
+                    <span className="react-edit-inline-label">{this.props.value}</span>
+                </div>
+            );
+        }
+
+        if (!this.state.isEdit) {
+            return (
+                <div className={cssClass}>
+                    <span className="react-edit-inline-label react-edit-inline-icon" onClick={this.edit.bind(this)}>{this.props.value}</span>
+                </div>
+            );
+        }
+
+        return (
+            <div className={cssClass} onKeyUp={this.onKeyUp.bind(this)}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default EditInline;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    },
+    {
+      test: /\.css/,
+      loaders: ['style', 'css']
+    },
+    {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
